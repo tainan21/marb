c54f31acb995643ed4f4e4ecc505df4687d8551a
Tai <tainancmoraes@gmail.com>
2022-09-05 11:37:04 -0300

    alter

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AboutView from '../views/aboutView.vue'
 import HomeView from '../views/HomeView.vue'
+import macanegra from '../views/macanegra.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/macanegra',
+    name: 'about',
+    component: macanegra
   },
 ]
 
---
+++ src/views/macanegra.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="onepage">
+      <div class="img1">
+        <div class="">
+          <img src="@/assets/garantia-maca.png" class="" alt="Responsive image" style="height: 70vh;">
+        </div>
+      </div>
+      <div class="img2">
+        <div class="ban-img">
+          <img src="@/assets/taisebn1.png" class="img-fluid taiseimage" alt="Responsive image" style=" height: 70vh;">
+        </div>
+      </div>
+</div>
+<div class="onepagebottom">
+    <div class="texto">Clique em Continuar Para Acessar o Site Oficial</div>
+    <div class="subtitle">MACA PERUANA NEGRA COM BORO ® Acesse o Site Oficial com Frete Grátis e até 45% de Desconto!</div>
+    <a href="" class="botao">Continuar</a>
+</div>
+
+</template>
+
+<style>
+.onepage{
+  width: 100vw;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+}
+.onepagebottom{
+  width: 100vw;
+  height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+}
+.img-1{
+  width: 80vw;
+  height: 80vh;
+}
+.img-2{
+  width: 80vw;
+  height: 80vh;
+}
+
+</Style>
